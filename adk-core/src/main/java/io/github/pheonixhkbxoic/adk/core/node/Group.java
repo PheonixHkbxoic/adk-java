14b66949b6001183d4ab1737d2e9b7505307d442
PheonixHkbxoic <hkbxoic@gmai.com>
2025-05-07 22:49:16 +0800

    reconstruct

---
+++ adk-core/src/main/java/io/github/pheonixhkbxoic/adk/core/node/Group.java
@@ -0,0 +1,18 @@
+package io.github.pheonixhkbxoic.adk.core.node;
+
+import io.github.pheonixhkbxoic.adk.core.spec.AbstractGroupNode;
+import io.github.pheonixhkbxoic.adk.core.spec.Node;
+
+/**
+ * @author PheonixHkbxoic
+ * @date 2025/5/4 22:49
+ * @desc
+ */
+public class Group extends AbstractGroupNode {
+
+    public Group(String name, Node entry) {
+        super(name, entry);
+    }
+
+    
+}
